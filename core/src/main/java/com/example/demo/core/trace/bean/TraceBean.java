9e8ecb2d99b1654012306eda33de19acec23b17e
fs19910227 <fs199102275219@gmail.com>
2020-08-03 00:11:26 +0800

    init 1

--- core/src/main/java/com/example/demo/core/trace/bean/TraceBean.java
+++ core/src/main/java/com/example/demo/core/trace/bean/TraceBean.java
@@ -3,8 +3,11 @@ package com.example.demo.core.trace.bean;
 import com.example.demo.core.util.LogUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,22 +16,35 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Slf4j
-public class TraceBean implements BeanPostProcessor, SmartInitializingSingleton {
+public class TraceBean implements BeanPostProcessor,
+        InitializingBean,
+        SmartInitializingSingleton, ApplicationContextAware {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        LogUtil.logTrace(log, this.getClass(), "postProcessBeforeInitialization");
+        LogUtil.logTrace(log, this.getClass(), "beforeInitialization,beanName is {}", beanName);
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        LogUtil.logTrace(log, this.getClass(), "postProcessBeforeInitialization");
-        return null;
+        LogUtil.logTrace(log, this.getClass(), "afterInitialization,beanName is {}", beanName);
+        return bean;
     }
 
     @Override
     public void afterSingletonsInstantiated() {
         LogUtil.logTrace(log, this.getClass(), "postProcessBeforeInitialization");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        LogUtil.logTrace(log, this.getClass(), "InitializingBean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        LogUtil.logTrace(log, this.getClass(), "aware ApplicationContext");
+
     }
 }
